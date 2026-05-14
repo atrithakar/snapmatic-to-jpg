c6aa1adda7f7253c66ba2b77c173da271d708f2e
Atri Thakar <atrithaker122@gmail.com>
2026-05-15 00:45:50 +0530

    Added function that automatically finds PGTA images

--- extract_gta_img.py
+++ extract_gta_img.py
@@ -1,12 +1,30 @@
 import os
 
-def extract_snapmatic():
-    files = [f for f in os.listdir('.') if f.startswith('PGTA') and not f.endswith('.jpg')]
+def find_pgta(start_path = os.path.expanduser('~')):
+    matches = []
 
-    if not files:
-        print("No files found to convert")
+    ignored_dirs = {'/proc', '/sys', '/dev', '/run', '.local/share/Trash'}
 
-    for file in files:
+    print(f"Searching for files at path {start_path}")
+    for root, dirs, files in os.walk(start_path):
+
+        if any(ignored in root for ignored in ignored_dirs):
+            continue
+
+        for file in files:
+            if file.startswith('PGTA') and '.' not in file and '_' not in file:
+                # print(f'found {file}')
+                matches.append(os.path.join(root, file))
+
+    return matches
+
+def extract_snapmatic(matches):
+
+    if not matches or len(matches) == 0:
+        print("No images found")
+        return
+
+    for file in matches:
         with open(file, 'rb') as f:
             data = f.read()
             jpeg_start = data.find(b'\xff\xd8\xff')
@@ -18,10 +36,24 @@ def extract_snapmatic():
 
                 with open(out_file, 'wb') as f_out:
                     f_out.write(clean_jpg_data)
-                    print("Extracted {out_file}")
+                    print(f"Extracted {out_file}")
             else:
                 print("failed to extract")
 
+def print_matches(matches):
+    if not matches or len(matches) == 0:
+        print("No images found")
+        return
+    
+    print('[')
+
+    for match in matches:
+        print(f'    {match},')
+
+    print(']')
 
 if __name__ == "__main__":
-    extract_snapmatic()
+    # extract_snapmatic()
+    # print(find_pgta('/home/atri/test_prefix/drive_c/users/atri/AppData/Roaming/Goldberg SocialClub Emu Saves/GTA V/0F74F4C4'))
+    print_matches(find_pgta('/home/atri/test_prefix/drive_c/users/atri/AppData/Roaming/Goldberg SocialClub Emu Saves/GTA V/0F74F4C4'))
+    # print(parse_pgta_header('/home/atri/test_prefix/drive_c/users/atri/AppData/Roaming/Goldberg SocialClub Emu Saves/GTA V/0F74F4C4/PGTA5519163650'))
