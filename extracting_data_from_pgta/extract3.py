f1b00f906c249bf6c87fbc3165a6e45b516b33b4
Atri Thakar <atrithaker122@gmail.com>
2026-05-15 11:54:07 +0530

    Refactored code for more efficiency and less repetition

--- extracting_data_from_pgta/extract3.py
+++ extracting_data_from_pgta/extract3.py
@@ -1,50 +1,34 @@
-import os
 import json
 
-def extractTitle(filepath):
-    with open(filepath, 'rb') as f:
-        data = f.read()
+def extract_pgta_data(filepath):
+    try:
+        with open(filepath, 'rb') as f:
+            data = f.read()
+    except FileNotFoundError:
+        return "Unknown", {}
 
-    # print(data)
-    titleIdx = data.index(b'TITL')
-    # title = data[start:end].
-    start = titleIdx + 8
-    end = data.find(b'\x00', start)
+    title = "Unknown"
+    json_obj = {}
 
-    title = data[start:end].decode('utf-8', errors='ignore')
-    return title
+    title_idx = data.find(b'TITL')
+    if title_idx != -1:
+        start = title_idx + 8
+        end = data.find(b'\x00', start)
+        if end != -1:
+            title = data[start:end].decode('utf-8', errors='ignore')
 
-def extractJSON(filepath):
-    with open(filepath, 'rb') as f:
-        data = f.read()
+    json_idx = data.find(b'{"loc"')
+    if json_idx != -1:
+        end = data.find(b'\x00', json_idx)
+        if end != -1:
+            json_string = data[json_idx:end].decode('utf-8', errors='ignore')
+            try:
+                json_obj = json.loads(json_string)
+            except json.JSONDecodeError:
+                pass 
 
-    # print(data)
-    start = data.index(b'{"loc"')
-    # title = data[start:end].
+    return title, json_obj
 
-    end = data.find(b'\x00', start)
 
-    json_data = data[start:end].decode('utf-8', errors='ignore')
-    return json.loads(json_data)
-
-    # return data[jsonIdx:jsonIdx+600].decode('utf-8', errors='ignore')
-
-def extractLocation(json):
-    return json['loc']
-
-def extractIngameTime(json):
-    return json['time']
-
-def extractRealWorldTimestamp(json):
-    return json['creat']
-
-def extractRadioStation(json):
-    return json['rds'] or 'none'
-
-json1 = extractJSON('/home/atri/test_prefix/drive_c/users/atri/AppData/Roaming/Goldberg SocialClub Emu Saves/GTA V/0F74F4C4/PGTA51098123832')
-json2 = extractJSON('/home/atri/Desktop/snapmatic_to_jpg/PGTA5236660974copy')
-
-print(f'{extractLocation(json1)}    {extractLocation(json2)}')
-print(f'{extractIngameTime(json1)}    {extractIngameTime(json2)}')
-print(f'{extractRealWorldTimestamp(json1)}    {extractRealWorldTimestamp(json2)}')
-print(f'{extractRadioStation(json1)}    {extractRadioStation(json2)}')
+def getJsonFields(json_obj, field_name, alt_value = 'none'):
+    return json_obj.get(field_name) or alt_value
